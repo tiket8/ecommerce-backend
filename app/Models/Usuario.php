e1641a2b1950515d0c6dbe35bbd8c756fa2ef024
tiket8 <tiket8@tiket8.com>
2024-08-05 21:33:30 -0700

    se configuro y probo con postman happy tokens

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Laravel\Sanctum\HasApiTokens; 
 class Usuario extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'nombre',
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\OfertaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,4 +27,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 });
 
+// Ruta para obtener las ofertas
 Route::get('ofertas', [OfertaController::class, 'index']);
